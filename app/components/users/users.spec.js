8fc55d5dc7b3b258b4a683cc2ae747436511d378
Chi Bui <Chi_bui@outlook.com>
2018-04-06 16:55:45 +1000

    add users component

---
+++ app/components/users/users.spec.js
@@ -0,0 +1,44 @@
+describe('UsersController', function () {
+
+    var $controller, UsersController, UsersFactory;
+
+    // Mock user list we expect to use in ctrl
+    var userList = [
+        { id: '1', name: 'Jane', role: 'Designer', location: 'New York', twitter: 'gijane' },
+        { id: '2', name: 'Bob', role: 'Developer', location: 'New York', twitter: 'billybob' },
+        { id: '3', name: 'Jim', role: 'Developer', location: 'Chicago', twitter: 'jimbo' },
+        { id: '4', name: 'Bill', role: 'Designer', location: 'LA', twitter: 'dabill' }
+    ];
+
+    beforeEach(angular.mock.module('ui.router'));
+
+    beforeEach(angular.mock.module('components.users'));
+    // add the module for Users serive
+    beforeEach(angular.mock.module('api.users'));
+
+    beforeEach(inject(function (_$controller_, _Users_) {
+
+        $controller = _$controller_;
+
+        UsersFactory = _Users_;
+
+        // spy and force the return value when UsersFactory.all() is called
+        spyOn(UsersFactory, 'all').and.callFake(function () {
+            return userList;
+        });
+
+        // add the factory as a dependency
+        UsersController = $controller('UsersController', { Users: UsersFactory });
+
+    }));
+
+    it('should be defined', function () {
+        expect(UsersController).toBeDefined();
+    });
+
+    // new test for expected ctrl behaviour
+    it('should initialize with a call to Users.all()', function () {
+        expect(UsersFactory.all).toHaveBeenCalled();
+        expect(UsersController.users).toEqual(userList);
+    });
+});
