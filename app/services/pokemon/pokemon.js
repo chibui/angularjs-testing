9dc0b424c409c0a1f8a694c2d3d7c68d71a3a111
Chi Bui <Chi_bui@outlook.com>
2018-05-02 11:47:08 +1000

    update pokemon api url to use https to avoid CORS

--- app/services/pokemon/pokemon.js
+++ app/services/pokemon/pokemon.js
@@ -4,7 +4,7 @@
     angular
         .module('api.pokemon', [])
         .factory('Pokemon', function ($http, $q) {
-            var API = 'http://pokeapi.co/api/v2/pokemon/',
+            var API = 'https://pokeapi.co/api/v2/pokemon/',
                 Pokemon = {};
 
             // spy on this method, chaining with callThrough() allows it to continue to $http.get()
